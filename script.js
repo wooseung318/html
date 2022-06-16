e5bf7c30e8724da6a3dcc76d5ab302912c509d27
wooseung318 <30212pcs@pcs.hs.kr>
2022-06-16 10:51:49 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+date.getFullYear();
+date.getMonth() + 1;
+
+currentYear % 4 === 0 && currentYear % 100 !== 0
+
+Math.ceil((currentDay + currentLastDate)/7);
+
+currentMonth - 1
+
+currentYear + 1
+
+<a href="#" class="button left" onclick="pcsCalendar(\''+id+'\',\''+prevDate+'\')">
+<a href="#" class="button right" onclick="pcsCalendar(\''+id+'\',\''+prevDate+'\')">
+
+calendar += '<tr>';
+
+dateNum++
+
+(dateNum < 1 || dateNum > currentLastDate)
+dateString[j]
+dateString[j]
+
+pcsCalendar.innerHTML=calendar;
